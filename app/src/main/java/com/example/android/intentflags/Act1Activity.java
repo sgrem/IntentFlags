191b9df011f90e9d15c335781d43935b9afbaad2
Steve Gremban <steve.gremban@gmail.com>
2017-10-25 16:11:17 -0500

    Create initial Android Studio Project

---
+++ app/src/main/java/com/example/android/intentflags/Act1Activity.java
@@ -0,0 +1,13 @@
+package com.example.android.intentflags;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Act1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act1);
+    }
+}
